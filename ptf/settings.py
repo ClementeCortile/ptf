866ac09b20273bd1c564a447f50d6cbf6c2aaa04
Clemente Cortile <clementecortile@gmail.com>
2019-04-23 15:21:27 +0200

    Adding Sections Part 2

--- ptf/settings.py
+++ ptf/settings.py
@@ -33,6 +33,9 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
+    'projects.apps.ProjectsConfig',
+    'works.apps.WorksConfig',
+    'presentations.apps.PresentationsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
