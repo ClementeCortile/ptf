1ad3857254ef10a7dcb06e5e40fb36b5da59c117
Clemente Cortile <clementecortile@gmail.com>
2019-06-23 13:45:48 +0200

    password change

--- ptf/settings.py
+++ ptf/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '123456',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
